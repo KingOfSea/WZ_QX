c0a67557581f5ebd96e250f855628982eb1283f6
KingOfSea <KingOfSea@users.noreply.github.com>
2022-06-04 15:55:17 +0800

    Up

--- js/feiniao.js
+++ js/feiniao.js
@@ -22,6 +22,25 @@ hostname = origin-prod-phoenix.jibjab.com
 **************************/
 
 let obj = JSON.parse($response.body);
-// obj = obj.data.attributes["data"];
-//obj.data.attributes["code"] = 0;
+
+obj.data.attributes["data"] = {
+    "max_parse_history" : 50,
+    "last_super_cnt" : 5,
+    "max_file_count" : 3000,
+    "max_file_size" : 0,
+    "show_new_version_dialog" : false,
+    "buy_card_discount" : 0,
+    "max_history" : 50,
+    "vip_exp_grow" : 15,
+    "web_version" : "1.7",
+    "buy_card_num" : 0,
+    "max_download_count" : 200,
+    "max_super_parse_cnt" : 5,
+    "file_count" : "0",
+    "has_import" : 0,
+    "max_save_count" : 30,
+    "is_vip" : 0,
+    "vip_exp_donate" : 30
+};
+
 $done({body: JSON.stringify(obj)});
