87d4f5ac1f6e2b4abc34c8b8832b77a5a78c0f38
KingOfSea <KingOfSea@users.noreply.github.com>
2022-08-21 01:42:29 +0800

    西窗烛解锁vip

---
+++ js/xcz.js
@@ -0,0 +1,23 @@
+/*
+# 西窗烛解锁vip
+
+***************************
+QuantumultX:
+
+^https:\/\/lchttpapi\.xczim\.com\/.+?\/users\/me url script-response-body https://raw.githubusercontent.com/KingOfSea/WZ_QX/master/js/xcz.js
+
+[mitm]
+hostname = lchttpapi.xczim.com
+
+**************************/
+
+var body = $response.body;
+var obj = JSON.parse($response.body);
+
+obj.didGiveOneMonthMembershipForQQAndroidPro = 1;
+obj.membership = 1;
+obj.lifetimeMembership = 1;
+obj.premiumMembership = 1;
+obj.isAdmin = 1;
+
+$done({ body: JSON.stringify(obj) });
