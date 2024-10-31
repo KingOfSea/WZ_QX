172e949e798029a970be07d7ec2ee1aa78a1fb75
wangzhihai <iosnoob@163.com>
2024-11-01 00:36:39 +0800

    up

--- js/hanmanjd.js
+++ js/hanmanjd.js
@@ -19,7 +19,7 @@ var body = $response.body;
 
 function removeCenterScript(input) {
     // 使用正则表达式匹配 <center> 和 <script> 标签之间的内容，允许有空格、换行等
-    const regex = /<center>[\s\S]*?<script>[\s\S]*?<\/script>[\s\S]*?<\/center>/g;
+    const regex = /<center><script>[\s\S]*?<\/script><\/center>/g;
     return input.replace(regex, '');
 }
 let cleanedHtml = removeCenterScript(body);
