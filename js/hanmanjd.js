39ee13715285575b6e6deb3008798d3ef0f3338a
wangzhihai <iosnoob@163.com>
2024-11-01 00:04:33 +0800

    up

--- js/hanmanjd.js
+++ js/hanmanjd.js
@@ -16,12 +16,19 @@ hostname = *.gqbyh.com, *.syzcny.com, *.hnhx360.com
 
 **************************/
 
-var body = $response.body;
-// 假设你已经有了包含上述HTML的元素
-const conchContent = document.querySelector('#conch-content');
-
-// 使用正则表达式找到并移除目标脚本标签
-const cleanedHtml = conchContent.innerHTML.replace(/<script.*?<\/script>/gs, '');
+function removeMaliciousTags(html) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    
+    // 移除所有包含特定类名的元素
+    const elements = doc.querySelectorAll('.palojsil_b');
+    elements.forEach(element => element.remove());
+    
+    return doc.body.innerHTML;
+}
+// 示例用法
+const originalHtml = body; // 在这里插入你的HTML字符串
+const cleanedHtml = removeMaliciousTags(originalHtml);
 
 // 将移除标签后的字符串返回
 console.log(cleanedHtml);
