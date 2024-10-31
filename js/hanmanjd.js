4bc2ab96a7ae8883b3cc8138c14645ce3db9ce73
wangzhihai <iosnoob@163.com>
2024-11-01 00:29:44 +0800

    up

--- js/hanmanjd.js
+++ js/hanmanjd.js
@@ -17,20 +17,12 @@ hostname = *.gqbyh.com, *.syzcny.com, *.hnhx360.com
 **************************/
 var body = $response.body;
 
-function removeMaliciousTags(html) {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-    
-    // 移除所有包含特定类名的元素
-    const elements = doc.querySelectorAll('.palojsil_b');
-    elements.forEach(element => element.remove());
-    
-    return doc.body.innerHTML;
+function removeCenterScript(input) {
+    // 使用正则表达式匹配 <center> 和 <script> 标签之间的内容，允许有空格、换行等
+    const regex = /<center>[\s\S]*?<script>[\s\S]*?<\/script>[\s\S]*?<\/center>/g;
+    return input.replace(regex, '');
 }
-// 示例用法
-const originalHtml = body; // 在这里插入你的HTML字符串
-const cleanedHtml = removeMaliciousTags(originalHtml);
-
+let cleanedHtml = removeCenterScript(body);
 // 将移除标签后的字符串返回
 console.log(cleanedHtml);
 
